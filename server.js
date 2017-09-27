d5353ea301e9226717d283528c202ec4e291f632
edzzh <edgarhart@inbox.lv>
2017-09-27 23:21:16 +0300

    Working handlebars on NodeJs

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
-let exphbs = require('express-handlebars');
+var path = require('path');
+var handlebars = require('express-handlebars');
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-	res.send('Hello!');
+	var requestData = {
+		identity: req.query.identity,
+		address: req.query.address
+	}
+	var templateData = {
+		year: new Date().getFullYear(),
+		article: 'Template will be right in the <code>views/</code> folder and layouts need to be in the <code>views/layouts/</code> folder'
+	}
+	res.render('home', templateData);
 });
 
-app.listen(3000);
+var server = app.listen(port, () => {
+	var port = server.address().port;
+	console.log('App running on http://localhost:%s', port);
+});
